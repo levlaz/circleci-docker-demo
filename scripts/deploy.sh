fa7834f41eb4e06aeb66703f1391b7bfecb22816
Lev Lazinskiy <lev@levlaz.org>
2016-12-14 10:05:47 -0800

    Run on port 80

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 ssh ubuntu@demo.levops.net 'docker pull levlaz/aws-demo'
 ssh ubuntu@demo.levops.net 'docker stop aws-demo'
-ssh ubuntu@demo.levops.net 'docker run --name aws-demo -d -p 5000:5000 levlaz/aws-demo'
+ssh ubuntu@demo.levops.net 'docker run --name aws-demo -d -p 80:5000 levlaz/aws-demo'
