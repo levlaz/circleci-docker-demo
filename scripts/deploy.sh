7116acbeb42164228a15a53afc4bcf82894c6d79
Lev Lazinskiy <lev@levlaz.org>
2017-04-18 07:52:56 -0500

    Test Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh ubuntu@demo.levops.net 'docker pull levlaz/aws-demo'
-ssh ubuntu@demo.levops.net 'docker stop aws-demo && docker rm aws-demo'
-ssh ubuntu@demo.levops.net 'docker run --name aws-demo -d -p 80:5000 levlaz/aws-demo'
+ssh ubuntu@demo.levlaz.org 'docker pull levlaz/aws-demo'
+ssh ubuntu@demo.levlaz.org 'docker stop aws-demo && docker rm aws-demo'
+ssh ubuntu@demo.levlaz.org 'docker run --name aws-demo -d -p 80:5000 levlaz/aws-demo'
